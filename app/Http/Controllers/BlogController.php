195c0a7a33d189168de5876f16f23cb40a02b1fa
DavidZakharov <lolola987@gmail.com>
2018-03-29 12:56:59 +0300

    Create an article search

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -1,11 +1,8 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Article;
 use App\Category;
 use Illuminate\Http\Request;
-
 class BlogController extends Controller
 {
     public function category($slug)
@@ -16,11 +13,19 @@ class BlogController extends Controller
           'articles' => $category->articles()->where('published', 1)->paginate(12)
         ]);
     }
-
     public function article($slug)
     {
-      return view('blog.article', [
-        'article' => Article::where('slug', $slug)->first()
-      ]);
+        return view('blog.article', [
+          'article' => Article::where('slug', $slug)->first()
+        ]);
+    }
+    public function search(Request $request)
+    {
+        $search = $request->input('search');
+        $article = Article::where('published', 1)->search($search)->paginate(12);
+        return view('blog.search', [
+          'search' => $search,
+          'articles' => $article
+        ]);
     }
 }
