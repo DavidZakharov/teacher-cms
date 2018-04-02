a3633fe9f21cfb1823a18c161861877a7415221e
DavidZakharov <lolola987@gmail.com>
2018-04-02 12:39:36 +0300

    Enable/disable user registration in config

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -24,11 +24,13 @@
 
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
-                <!-- Authentication Links -->
-                @guest
-                    <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}">Register</a></li>
-                @else
+              <!-- Authentication Links -->
+               @guest
+                   <li><a href="{{ route('login') }}">Login</a></li>
+                   @if (config('auth.registration'))
+                     <li><a href="{{ route('register') }}">Register</a></li>
+                   @endif
+               @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                             {{ Auth::user()->name }} <span class="caret"></span>
