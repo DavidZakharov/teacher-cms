f16557f03923c7cf943a43c434be0cef7fe8c5ce
DavidZakharov <lolola987@gmail.com>
2018-04-06 11:19:01 +0300

    Create gate: admin-only

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -17,30 +17,32 @@
         </div>
 
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
-          <!-- Left Side Of Navbar -->
-          <ul class="nav navbar-nav">
-              @include('layouts.top_menu_categories', ['categories' => $categories])
-              @include('layouts.top_menu_pages', ['pages' => $pages])
-          </ul>
+            <!-- Left Side Of Navbar -->
+            <ul class="nav navbar-nav">
+                @include('layouts.top_menu_categories', ['categories' => $categories])
+                @include('layouts.top_menu_pages', ['pages' => $pages])
+            </ul>
 
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
-              <!-- Authentication Links -->
-               @guest
-                   <li><a href="{{ route('login') }}">Login</a></li>
-                   @if (config('auth.registration'))
-                     <li><a href="{{ route('register') }}">Register</a></li>
-                   @endif
-               @else
+                <!-- Authentication Links -->
+                @guest
+                    <li><a href="{{ route('login') }}">Login</a></li>
+                    @if (config('auth.registration'))
+                      <li><a href="{{ route('register') }}">Register</a></li>
+                    @endif
+                @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
                         <ul class="dropdown-menu">
-                            <li>
-                              <a href="{{ route('admin.index') }}">Admin panel</a>
-                            </li>
+                            @can('admin-only', auth()->user())
+                              <li>
+                                <a href="{{ route('admin.index') }}">Admin panel</a>
+                              </li>
+                            @endcan
                             <li>
                                 <a href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
